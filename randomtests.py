b1c42925eba0d5a70da897a44e31db33fa38db77
ajmalesa <u0766202@umail.utah.edu>
2018-03-31 20:04:34 -0600

    Initial Commit

---
+++ randomtests.py
@@ -0,0 +1,14 @@
+import random
+
+
+def returnTrueForPercentage (percentage):
+    if(random.randrange(0, 101) <= percentage):
+        return True
+
+
+totalIterations = 100
+for i in range(totalIterations):
+    if(returnTrueForPercentage(70)):
+
+
+print(total / 100)
